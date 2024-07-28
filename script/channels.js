281983964f3bb25c8aa4cd457faf6b9f4e3094bf
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-07-28 22:55:49 +0300

    Панель каналов

--- script/channels.js
+++ script/channels.js
@@ -2,6 +2,22 @@ let channels = document.body.getElementsByClassName('channels')[0];
 
 for( i=0; i<data_channels.length; i++ ){
     channels.innerHTML += `
-    <div>${data_channels[i]}</div>
+        <div class="channel">
+            <div class="icon" style="background-image: url('svg/login.svg')"></div>
+            <div class="name">Name channel</div>
+            <div class="user">
+                <div>191687</div>
+                <div>участников</div>
+            </div>
+            <div class="send">
+                <div>1.6m</div>
+                <div>сообщений</div>
+            </div>
+            <div class="mau">
+                <div>44.5k</div>
+                <div>MAU</div>
+            </div>
+            <div>#1</div>
+        </div>
 `
 }
