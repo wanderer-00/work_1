07a8cee07bcb922bebde72651a8c5f193cb2a827
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2024-07-28 22:21:10 +0300

    Компоненты реализованы

---
+++ script/footer.js
@@ -0,0 +1,71 @@
+let footer = document.body.getElementsByTagName('footer')[0];
+
+footer.innerHTML = `
+            <div class="wrapper">
+                <div class="table">
+
+                    <div>
+                        <div class="title">Каталог</div>
+                        <a href="#">Каталог каналов и чатов</a>
+                        <a href="#">Подборки каналов</a>
+                        <a href="#">Поиск каналов</a>
+                        <a href="#">Добавить канал/чат</a>
+                    </div>
+
+                    <div>
+                        <div class="title">Рейтинги</div>
+                        <a href="#">Рейтинг каналов Telegram</a>
+                        <a href="#">Рейтинг чатов Telegram</a>
+                        <a href="#">Рейтинг публикаций</a>
+                        <a href="#">Рейтинги брендов и персон</a>
+                    </div>
+
+                    <div>
+                        <div class="title">API</div>
+                        <a href="#">API статистики</a>
+                        <a href="#">API поиска публикаций</a>
+                        <a href="#">API Callback</a>
+                    </div>
+
+                    <div>
+                        <div class="title">Наши каналы</div>
+                        <a href="#">@канал_1</a>
+                        <a href="#">@канал_2</a>
+                        <a href="#">@канал_3</a>
+                        <a href="#">@канал_4</a>
+                    </div>
+
+                    <div>
+                        <div class="title">Почитать</div>
+                        <a href="#">Наш блог</a>
+                        <a href="#">Исследование Telegram 2019</a>
+                        <a href="#">Исследование Telegram 2021</a>
+                        <a href="#">Исследование Telegram 2023</a>
+                    </div>
+
+                    <div>
+                        <div class="title">Контакты</div>
+                        <a href="#">Поддержка</a>
+                        <a href="#">Почта</a>
+                        <a href="#">Вакансии</a>
+                    </div>
+
+                    <div>
+                        <div class="title">Всякая всячина</div>
+                        <a href="#">Пользовательское соглашение</a>
+                        <a href="#">Политика конфиденциальности</a>
+                        <a href="#">Публичная оферта</a>
+                    </div>
+
+                    <div>
+                        <div class="title">Наши боты</div>
+                        <a href="#">@bot_1</a>
+                        <a href="#">@bot_2</a>
+                        <a href="#">@bot_3</a>
+                        <a href="#">@bot_4</a>
+                        <a href="#">@bot_5</a>
+                    </div>
+
+                </div>
+            </div>
+`;
